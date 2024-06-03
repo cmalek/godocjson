6c19ee2b96be5a5ae130154ce006dfa8fba52d37
Chris Malek <cmalek@caltech.edu>
2024-06-03 14:26:29 -0700

    Bump version: 1.0.0 → 1.0.1

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 
 APP_NAME=godocjson
 
-VERSION := "v1.0.0"
+VERSION := "v1.0.1"
 PACKAGE := "godocjson"
 
 default: build
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v1.0.0"
+VERSION="v1.0.1"
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
